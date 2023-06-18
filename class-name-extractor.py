bf84a06d9027d41c2cde831415cc97096c2410ac
François Schiettecatte <fschiettecatte@gmail.com>
2023-06-18 15:11:12 -0700

    Update class-name-extractor.py

--- class-name-extractor.py
+++ class-name-extractor.py
@@ -7,12 +7,9 @@ class_name_set = set()
 
 with open('bootstrap.css') as file_handle:
     for line in file_handle:
-        line = line.strip()
-        if line.startswith('.'):
-            for class_name in re.split(r'[\s,\{]+', line):
-                class_name = class_name.split(':')[0]
-                if class_name.startswith('.'):
-                    class_name_set.add(class_name)
-            
+        line = re.sub(r'".*?"', '', line)
+        for class_name in re.findall(r'(\.[a-z][a-z0-9\-]+)', line):
+            class_name_set.add(class_name)
+
 print('\n'.join(sorted(class_name_set)))
 
